2283f505face220d430835aee94997233988cf01
Xytronix <32957125+Xytronix@users.noreply.github.com>
2026-03-13 12:01:02 +0100

    Fix connection timeouts by implementing Pong responses
    Respond to Hytale Ping packets with Pong for all PongType values (Raw, Direct, Tick) to prevent keepalive timeout disconnects.

--- core/src/main/java/es/edwardbelt/hycraft/network/handler/minecraft/play/KeepAliveResponseHandler.java
+++ core/src/main/java/es/edwardbelt/hycraft/network/handler/minecraft/play/KeepAliveResponseHandler.java
@@ -1,5 +1,8 @@
 package es.edwardbelt.hycraft.network.handler.minecraft.play;
 
+import com.hypixel.hytale.protocol.packets.connection.Ping;
+import com.hypixel.hytale.protocol.packets.connection.Pong;
+import com.hypixel.hytale.protocol.packets.connection.PongType;
 import es.edwardbelt.hycraft.network.handler.PacketHandler;
 import es.edwardbelt.hycraft.network.player.ClientConnection;
 import es.edwardbelt.hycraft.protocol.packet.play.KeepAliveResponsePacket;
@@ -7,13 +10,13 @@ import es.edwardbelt.hycraft.protocol.packet.play.KeepAliveResponsePacket;
 public class KeepAliveResponseHandler implements PacketHandler<KeepAliveResponsePacket> {
     @Override
     public void handle(KeepAliveResponsePacket packet, ClientConnection connection) {
-        /*Ping pendingPing = connection.getPendingPing();
+        Ping pendingPing = connection.getPendingPing();
         if (pendingPing != null) {
             for (PongType type : PongType.VALUES) {
                 Pong pongPacket = new Pong(pendingPing.id, pendingPing.time, type, (short) 0);
                 connection.getHytaleChannel().sendPacket(pongPacket);
             }
             connection.setPendingPing(null);
-        }*/
+        }
     }
 }
